cae00c710ea78f280ce0f0e39d6044671884b3b9
Supriya2326 <234g1a33h3@srit.ac.in>
2025-02-08 21:03:49 +0530

    Add files via upload

---
+++ ConDemo.java
@@ -0,0 +1,18 @@
+class ConDemo
+{
+int i,j;
+ConDemo()
+{
+i=48;
+j=56;
+}
+public void display()
+{
+System.out.println("value of i:"+i+"and j:"+j);
+}
+public static void main(String args[])
+{
+ConDemo obj=new ConDemo();
+obj.display();
+}
+}
