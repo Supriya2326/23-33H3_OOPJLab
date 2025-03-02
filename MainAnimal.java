aae6d0ab25da5d0cf26f49345d80e5d312d78b28
Supriya2326 <234g1a33h3@srit.ac.in>
2025-03-02 23:14:37 +0530

    Add files via upload

---
+++ MainAnimal.java
@@ -0,0 +1,37 @@
+// Base class
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+// Derived class 1
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+// Derived class 2
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class MainAnimal {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        animal.sound(); 
+        dog.sound();    
+        cat.sound();     
+    }
+}
+
+
+
